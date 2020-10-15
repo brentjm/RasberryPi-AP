1816e8e7006f3a8dc13179bf887b6e82fbcf9705
brentjm <brent_maranzano@yahoo.com>
2020-10-15 11:40:09 +0100

    Added user input to capture network specific options

--- AP_setup.sh
+++ AP_setup.sh
@@ -9,55 +9,60 @@
 # Created: Brent Maranzano
 # Last modified: June 17, 2020
 
-# Configuation modfifications:
-# 1. dnsmasq.conf to change the network settings
-#   a. served IP range (dhcpcd.conf also)
-#   b. domain name
-# 2. hostapd.conf to change WiFi settings
-#   a. SSID
-#   b. WiFi password
-#   c. radio band
-#   d. encryption settings
-# 3. dhcpcd.conf
-#   a. IP range (dnsmasq.conf also)
-
-
 # Example to run (as root):
 # root>./AP_setup.sh
 ############################################################
+read -p 'Input the 3rd octet of the network IP address (IP -> 192.168.$IP.xxx/24): ' IP
+read -p 'Input the SSID: ' SSID
+read -p 'Input the channel: ' CHANNEL
+read -p 'Input the password: ' PASSWORD
 
-# Install hostapd and dnsmasq.
-DEBIAN_FRONTEND=noninteractive apt install -y hostapd dnsmasq
+echo "Append the wireless configuration into a temporary dhcpcd.conf file"
+cat /etc/dhcpcd.conf > dhcpcd.conf
+sed -i -e '$ainterface wlan0' dhcpcd.conf
+sed -i -e '$a\ \ \ \ \ static ip_address=192.168.'"$IP"'.1/24' dhcpcd.conf
+sed -i -e '$a\ \ \ \ \ nohook wpa_supplicant' dhcpcd.conf
+sed -i -e '$a\ \ \ \ \ denyinterfaces wlan0' dhcpcd.conf
 
-# Start the hostapd service on boot.
+echo "Create the dnsmasq.conf file from the template."
+sed -e 's/number/'"$IP"'/' dnsmasq.conf_template > dnsmasq.conf
+
+echo "Create the hostapd.conf file from the template."
+sed -e 's/NameOfNetwork/'"$SSID"'/' hostapd.conf_template > hostapd.conf
+sed -i -e 's/ChannelNumber/'"$CHANNEL"'/' hostapd.conf
+sed -i -e 's/WiFiPassword/'"$PASSWORD"'/' hostapd.conf
+
+echo "/n Install packages hostapd dnsmasq netfilter-persistent iptables-persistent"
+DEBIAN_FRONTEND=noninteractive apt install -y hostapd dnsmasq netfilter-persistent iptables-persistent
+
+echo "Start the hostapd service on boot."
 systemctl unmask hostapd
 systemctl enable hostapd
 
-# Append the dhcp configuration into the system file.
-# Changed from cat to sed, easier to confirm the reverse
-#cat dhcpcd.conf >> /etc/dhcpcd.conf
-sed -i -e '$ainterface wlan0' /etc/dhcpcd.conf
-sed -i -e '$a    static ip_address=192.168.4.1/24' /etc/dhcpcd.conf
-sed -i -e '$a    nohook wpa_supplicant' /etc/dhcpcd.conf
-sed -i -e '$a    denyinterfaces wlan0' /etc/dhcpcd.conf
-
-# Uncomment the port forwarding rule.
+echo "Uncomment the port forwarding rule."
 sed -i -e "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 
-# Enable port forwarding and save.
-iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE || iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables-save > /etc/iptables.ipv4.nat
-sed -e '/exit 0/iiptables-restore < /etc/iptables.ipv4.nat/' /etc/rc.local
+echo "Enable port forwarding and save."
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+netfilter-persistent save
+#iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE || iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#iptables-save > /etc/iptables.ipv4.nat
+#sed -e '/exit 0/iiptables-restore < /etc/iptables.ipv4.nat/' /etc/rc.local
 
-# Backup the old DNS configuration and copy over the new configuration.
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-cp dnsmasq.conf /etc/dnsmasq.conf
-
-# Unblock any radio signals.
+echo "Unblock any radio signals."
 rfkill unblock wlan
 
-# Copy over the hostapd configuration file.
-cp hostapd.conf /etc/hostapd/hostapd.conf
+echo "Backup the old dhcpcd.conf file and copy over the new configuration file."
+mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+mv dhcpcd.conf /etc/dhcpcd.conf
 
-# Reboot the system.
+echo "Backup the old dnsmasq.conf file and copy over the new configuration file."
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+mv dnsmasq.conf /etc/dnsmasq.conf
+
+echo "Backup the old hostapd.conf file and copy over the new configuration file."
+mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.orig
+mv hostapd.conf /etc/hostapd/hostapd.conf
+
+echo "Reboot the system."
 systemctl reboot
