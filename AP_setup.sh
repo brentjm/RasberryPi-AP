4f694585739ea247a8ce9b133901b0d9085814c5
brentjm <brent_maranzano@yahoo.com>
2020-10-16 12:04:49 +0100

    Working version

--- AP_setup.sh
+++ AP_setup.sh
@@ -17,23 +17,35 @@ read -p 'Input the SSID: ' SSID
 read -p 'Input the channel: ' CHANNEL
 read -p 'Input the password: ' PASSWORD
 
-echo "Append the wireless configuration into a temporary dhcpcd.conf file"
+echo "/n Install packages hostapd dnsmasq netfilter-persistent iptables-persistent"
+DEBIAN_FRONTEND=noninteractive apt install -y hostapd dnsmasq netfilter-persistent iptables-persistent
+
+echo "/nAppend the wireless configuration into a temporary dhcpcd.conf file"
 cat /etc/dhcpcd.conf > dhcpcd.conf
 sed -i -e '$ainterface wlan0' dhcpcd.conf
 sed -i -e '$a\ \ \ \ \ static ip_address=192.168.'"$IP"'.1/24' dhcpcd.conf
 sed -i -e '$a\ \ \ \ \ nohook wpa_supplicant' dhcpcd.conf
-sed -i -e '$a\ \ \ \ \ denyinterfaces wlan0' dhcpcd.conf
+#sed -i -e '$a\ \ \ \ \ denyinterfaces wlan0' dhcpcd.conf
 
 echo "Create the dnsmasq.conf file from the template."
-sed -e 's/number/'"$IP"'/' dnsmasq.conf_template > dnsmasq.conf
+sed -e 's/Number/'"$IP"'/g' dnsmasq.conf_template > dnsmasq.conf
 
 echo "Create the hostapd.conf file from the template."
 sed -e 's/NameOfNetwork/'"$SSID"'/' hostapd.conf_template > hostapd.conf
 sed -i -e 's/ChannelNumber/'"$CHANNEL"'/' hostapd.conf
 sed -i -e 's/WiFiPassword/'"$PASSWORD"'/' hostapd.conf
 
-echo "/n Install packages hostapd dnsmasq netfilter-persistent iptables-persistent"
-DEBIAN_FRONTEND=noninteractive apt install -y hostapd dnsmasq netfilter-persistent iptables-persistent
+echo "Backup the old dhcpcd.conf file and copy over the new configuration file."
+mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+mv dhcpcd.conf /etc/dhcpcd.conf
+
+echo "Backup the old dnsmasq.conf file and copy over the new configuration file."
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+mv dnsmasq.conf /etc/dnsmasq.conf
+
+echo "Backup the old hostapd.conf file and copy over the new configuration file."
+mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.orig
+mv hostapd.conf /etc/hostapd/hostapd.conf
 
 echo "Start the hostapd service on boot."
 systemctl unmask hostapd
@@ -51,18 +63,6 @@ netfilter-persistent save
 
 echo "Unblock any radio signals."
 rfkill unblock wlan
-
-echo "Backup the old dhcpcd.conf file and copy over the new configuration file."
-mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
-mv dhcpcd.conf /etc/dhcpcd.conf
-
-echo "Backup the old dnsmasq.conf file and copy over the new configuration file."
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-mv dnsmasq.conf /etc/dnsmasq.conf
-
-echo "Backup the old hostapd.conf file and copy over the new configuration file."
-mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.orig
-mv hostapd.conf /etc/hostapd/hostapd.conf
 
 echo "Reboot the system."
 systemctl reboot
